436728b29d55fcf15c5a5bc25d41f9b551596563
Mateusz Tadla <mat@scalepoint.com>
2022-10-22 16:59:42 +0200

    progress bar ex1

--- src/test/java/ProgressBarTests.java
+++ src/test/java/ProgressBarTests.java
@@ -41,6 +41,8 @@ public class ProgressBarTests extends TestBase{
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
+        wait.until(ExpectedConditions.textToBePresentInElement(progressBarLabel, "Complete!"));
+
     }
 
     @Test
