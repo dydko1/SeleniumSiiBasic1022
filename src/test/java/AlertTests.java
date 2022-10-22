2710a386cbfc7dc30ec671539d1913291e61b9b0
Mateusz Tadla <mat@scalepoint.com>
2022-10-22 14:56:22 +0200

    add ifrem test

--- src/test/java/AlertTests.java
+++ src/test/java/AlertTests.java
@@ -14,4 +14,16 @@ public class AlertTests extends TestBase{
         String alertMsg = driver.findElement(By.id("simple-alert-label")).getText();
         Assert.assertEquals(alertMsg, "OK button pressed");
     }
+
+    @Test
+    public void shouldAcceptPromptAlert(){
+        driver.get("http://51.75.61.161:9102/alerts.php");
+        driver.findElement(By.id("prompt-alert")).click();
+
+        driver.switchTo().alert().sendKeys("Geralt");
+        driver.switchTo().alert().accept();
+
+        String alertMsg = driver.findElement(By.id("prompt-label")).getText();
+        Assert.assertEquals(alertMsg, "Hello Geralt! How are you today?");
+    }
 }
---
+++ src/test/java/IframeTest.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class IframeTest extends TestBase{
+
+    @Test
+    public void shouldFillFormInIframe(){
+        driver.get("http://automation-practice.emilos.pl/iframes.php");
+
+
+        driver.findElement(By.className("dropdown")).getText();
+
+        driver.switchTo().frame("iframe1");
+
+        driver.findElement(By.id("inputFirstName3")).sendKeys("Jan");
+
+        driver.switchTo().defaultContent();
+        driver.findElement(By.className("dropdown")).getText();
+
+    }
+}
