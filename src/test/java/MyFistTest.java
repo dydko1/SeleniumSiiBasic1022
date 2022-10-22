2fe858cefd09eb2742a1f50ce7b0a014bae91c74
Mateusz Tadla <mat@scalepoint.com>
2022-10-22 09:59:37 +0200

    add my fist test

---
+++ src/test/java/MyFistTest.java
@@ -0,0 +1,29 @@
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MyFistTest {
+
+
+    @BeforeMethod
+    public void runBeforeEach(){
+        System.out.println("uruchomie sie zawsze");
+    }
+
+    @Test
+    public void test1(){
+        Assert.assertEquals(add(2,2), 4);
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("xx");
+        int a = 10;
+        Assert.assertEquals(a,5);
+        System.out.println("yy");
+    }
+
+    public int add(int a, int b){
+        return a+b;
+    }
+}
