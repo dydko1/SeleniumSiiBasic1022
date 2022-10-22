bc6f2b92a4d218fc0f254ef8193e3e8050d0d84d
Mateusz Tadla <mat@scalepoint.com>
2022-10-22 11:02:07 +0200

    add test base

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -3,7 +3,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class MyFistTest {
+public class MyFirstTest {
     @BeforeMethod
     public void runBeforeEach(){
         System.out.println("run before each");
