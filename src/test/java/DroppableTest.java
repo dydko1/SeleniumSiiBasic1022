1ec9998c7c943f155353542e5f2c67293aebb997
Mateusz Tadla <mat@scalepoint.com>
2022-10-22 15:51:19 +0200

    actions examnple

---
+++ src/test/java/DroppableTest.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class DroppableTest extends TestBase{
+
+    @Test
+    public void shouldDragAndDrop(){
+        driver.get("http://51.75.61.161:9102/droppable.php");
+
+        WebElement toDrag = driver.findElement(By.id("draggable"));
+        WebElement dropTo = driver.findElement(By.id("droppable"));
+
+        Actions actions = new Actions(driver);
+
+        // opcja 1
+//        actions.clickAndHold(toDrag)
+//                .moveToElement(dropTo)
+//                .release()
+//                .perform( );
+
+//        opcja 2
+//        actions.clickAndHold(toDrag)
+//                .release(dropTo)
+//                .perform();
+
+        actions.dragAndDrop(toDrag, dropTo).perform();
+    }
+}
