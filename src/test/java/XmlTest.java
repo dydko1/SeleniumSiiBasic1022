1823b913cb0bd5c1f5e65f7214c605ccebc239b1
dydko <miroslaw.dyduch@gmail.com>
2023-02-02 13:16:32 +0100

    fsdf

---
+++ src/test/java/XmlTest.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.Properties;
+
+public class XmlTest {
+
+    @Test
+    @Parameters({"browser"})
+    public void test(@Optional("hello Mirkuuuuu") String browser) {
+        Properties properties = new Properties();
+        String s = System.getProperty("browser");
+        //properties.getProperty("browser");
+
+        System.out.println("test param=" + browser);
+        System.out.println("browser=" + s);
+
+    }
+}
